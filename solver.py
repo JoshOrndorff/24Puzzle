48b74efd6933058e284ec1781a24888070a7f1a2
Joshy Orndorff <admin@joshorndorff.com>
2018-05-30 11:31:24 -0400

    Use Dijkstra's algorithm to avoid dupes and improve runtime.

--- solver.py
+++ solver.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 
+numbers = 4
 target = 24
 threshold = .0000001
-solutions = []
 
 class chunk(object):
   def __init__(self, number, text = None):
@@ -11,6 +11,19 @@ class chunk(object):
       self.text = str(number)
     else:
       self.text = text
+
+  # For sorting
+  def __lt__(self, other):
+    return self.total < other.total
+
+  def __eq__(self, other):
+    return self.text == other.text
+
+  def __hash__(self):
+    return hash(self.text)
+
+  def __str__(self):
+    return self.text
 
 
 def add(a, b):
@@ -44,16 +57,14 @@ def operate(chunks):
   if len(chunks) == 1:
     # All compressed down to one chunk
     if (chunks[0].total - target < threshold) and \
-       (target - chunks[0].total < threshold) and \
-       (chunks[0] not in solutions):
-       
-      solutions.append(chunks[0])
-    return
-  
+       (target - chunks[0].total < threshold):
+
+      print(chunks[0])
+
   else:
   # Some chunks still remain
     for chunk1 in chunks:
-      chunksM1 = chunks.copy()
+      chunksM1 = list(chunks) # Makes a copy
       chunksM1.remove(chunk1)
       for chunk2 in chunksM1:
         chunksM2 = chunksM1.copy()
@@ -66,8 +77,9 @@ def operate(chunks):
           else:
             newChunks = chunksM2.copy()
             newChunks.append(newChunk)
-            operate(newChunks)
-        
+            newChunks.sort() # Should be O(n) because it was already sorted
+            open.add(tuple(newChunks))
+
 
 def number_input(prompt):
   num = input(prompt)
@@ -79,13 +91,21 @@ def number_input(prompt):
       
 if __name__ == "__main__":
 
-  numbers = []
-  for n in range(4):
-    numbers.append(chunk(number_input("Enter a puzzle number: ")))  
-  operate(numbers)
-    
-  if len(solutions) == 0:
-    print("No solutions found.")
-  else:
-    for solution in solutions:
-      print(solution.text)
+  # Get the user input and initialize things
+  operations = (add, multiply, subtract, divide)
+  closed = set()
+  open = set()
+  initial = []
+  for i in range(numbers):
+    initial.append(chunk(number_input("Enter a puzzle number: ")))
+  initial.sort()
+
+  open.add(tuple(initial))
+
+  # Execute the solution search
+  while len(open) > 0:
+    #TODO remove debugging line input("open: {} closed: {}".format(len(open), len(closed)))
+    chunks = open.pop()
+    if chunks not in closed:
+      operate(chunks)
+    closed.add(chunks)
